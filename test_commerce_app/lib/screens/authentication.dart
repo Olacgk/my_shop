d7dbe487070e3c40340bde94ba0284edfef003c8
Dell <dell@grafico.com>
2023-06-01 17:14:51 +0100

    authentication page correct

--- test_commerce_app/lib/screens/authentication.dart
+++ test_commerce_app/lib/screens/authentication.dart
@@ -1,73 +1,85 @@
 import 'package:flutter/material.dart';
 
 class Authentication extends StatefulWidget {
-  const Authentication({Key? key}) : super (key: key);
+  const Authentication({Key? key}) : super(key: key);
 
   @override
-  State<Authentication> createState() => _AuthenticationState();
+  _AuthenticationState createState() => _AuthenticationState();
 }
 
 class _AuthenticationState extends State<Authentication> {
-
   final _formKey = GlobalKey<FormState>();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/Nature-Quotes.png'),
-            fit: BoxFit.contain,
-          ),
-        ),
-        child: Center(
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 400),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(height: 100,),
-              const Text("LOGO", style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold), textAlign: TextAlign.center, ),
-              const SizedBox(height: 150,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Text("Username"),
-                            const SizedBox(width: 10,),
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                icon: Icon(Icons.person, color: Colors.black),
-                                hintText: 'username...',
-                                contentPadding: EdgeInsets.symmetric(
-                                    vertical: 10.0, horizontal: 8.0),
-                              ),
-                            )
-                          ],
+              const Text(
+                "LOGO",
+                style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 50),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Username"),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.person, color: Colors.black),
+                        hintText: 'Username...',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your username';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    const Text("Password"),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.lock, color: Colors.black),
+                        hintText: 'Password...',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            // Form is valid, perform authentication
+                            // Implement your authentication logic here
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.black26
                         ),
-                        const SizedBox(height: 50,),
-                        Row(
-                          children: [
-                            const Text("Password"),
-                            const SizedBox(width: 10,),
-                            TextFormField(
-                              obscureText: true,
-                              decoration: const InputDecoration(
-                                icon: Icon(Icons.lock, color: Colors.black),
-                                hintText: 'password...',
-                                contentPadding: EdgeInsets.symmetric(
-                                    vertical: 10.0, horizontal: 8.0),
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    )),
-              )
+                        child: const Text('Login'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
